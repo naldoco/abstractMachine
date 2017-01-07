ee0204cf1402b1f08cc2cdcbd158c15097dc1609
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2017-01-07 22:27:14 +0100

    Test value'

--- test/Tests.hs
+++ test/Tests.hs
@@ -8,6 +8,12 @@ import AbstractMachine
 exp1 :: Expr
 exp1 = Val 4
 
+exp2 :: Expr
+exp2 = Add (Add (Val 2) (Val 3)) (Val 4)
+
+exp3 :: Expr
+exp3 = Add (Val 4) (Add (Val 2) (Val 3))
+
 abstractMachineSuite :: TestTree
 abstractMachineSuite =
   testGroup "AbstractMachine tests"
@@ -18,6 +24,9 @@ abstractMachineSuite =
     , testGroup "value'"
         [ testCase ("value' "++(show exp1)++" -> "++show 4) $
             (value' exp1) @?= 4
-        ]
+        , testCase ("value' "++(show exp2)++" -> "++show 9) $
+            (value' exp2) @?= 9
+        , testCase ("value' "++(show exp3)++" -> "++show 9) $
+            (value' exp3) @?= 9        ]
     ]
 main = defaultMain abstractMachineSuite
