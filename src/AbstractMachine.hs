74779d7244ff89ce21eabdeb19557dceaa62435f
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2017-01-07 21:52:36 +0100

    First Red

---
+++ src/AbstractMachine.hs
@@ -0,0 +1,6 @@
+module AbstractMachine where
+
+data Expr = Val Int | Add Expr Expr deriving (Show)
+
+value :: Expr -> Int
+value = undefined
---
+++ test/Tests.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import AbstractMachine
+
+exp1 :: Expr
+exp1 = Val 4
+
+abstractMachineSuite :: TestTree
+abstractMachineSuite =
+  testGroup "AbstractMachine tests"
+    [ testGroup "value"
+        [ testCase ("value "++(show exp1)++" -> "++show 4) $
+            (value exp1) @?= 4
+        ]
+    ]
+main = defaultMain abstractMachineSuite
