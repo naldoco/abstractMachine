e2b29c7954064b720037e51ba189073c46e95026
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2017-01-08 01:55:26 +0100

    Red. Add eval, exec

--- src/AbstractMachine.hs
+++ src/AbstractMachine.hs
@@ -1,6 +1,8 @@
 module AbstractMachine where
 
 data Expr = Val Int | Add Expr Expr deriving (Show)
+type Cont = [Op]
+data Op   = EVAL Expr | ADD Int deriving (Show)
 
 value :: Expr -> Int
 value _ = 4
@@ -8,3 +10,9 @@ value _ = 4
 value' :: Expr -> Int
 value' (Val n) = n
 value' (Add x y) = value' x + value' y
+
+eval :: Expr -> Cont -> Int
+eval = undefined
+
+exec :: Cont -> Int -> Int
+exec = undefined
--- test/Tests.hs
+++ test/Tests.hs
@@ -14,6 +14,12 @@ exp2 = Add (Add (Val 2) (Val 3)) (Val 4)
 exp3 :: Expr
 exp3 = Add (Val 4) (Add (Val 2) (Val 3))
 
+op1 :: Op
+op1 = ADD 5
+
+co1 :: Cont
+co1 = [op1]
+
 abstractMachineSuite :: TestTree
 abstractMachineSuite =
   testGroup "AbstractMachine tests"
@@ -27,6 +33,15 @@ abstractMachineSuite =
         , testCase ("value' "++(show exp2)++" -> "++show 9) $
             (value' exp2) @?= 9
         , testCase ("value' "++(show exp3)++" -> "++show 9) $
-            (value' exp3) @?= 9        ]
+            (value' exp3) @?= 9
+        ]
+    , testGroup "eval"
+        [ testCase ("eval "++(show exp1)++" "++(show co1)++" -> "++show 9) $
+            (eval exp1 co1) @?= 9
+        ]
+    , testGroup "exec"
+        [ testCase ("exec "++(show co1)++" "++(show 4)++" -> "++show 9) $
+            (exec co1 4) @?= 9
+        ]
     ]
 main = defaultMain abstractMachineSuite
