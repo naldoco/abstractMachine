f93347f3cf7e09eacd67f91e3a64d48b830226d8
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2017-01-08 01:56:14 +0100

    Green. Add eval, exec

--- src/AbstractMachine.hs
+++ src/AbstractMachine.hs
@@ -12,7 +12,10 @@ value' (Val n) = n
 value' (Add x y) = value' x + value' y
 
 eval :: Expr -> Cont -> Int
-eval = undefined
+eval (Val n)   c = exec c n
+eval (Add x y) c = eval x (EVAL y : c)
 
 exec :: Cont -> Int -> Int
-exec = undefined
+exec [] n           = n
+exec (EVAL y : c) n = eval y (ADD n : c)
+exec (ADD  n : c) m = exec c (n + m)
